aa141e4019d0add5184ef204acd6118f31438758
Martin René Sørensen <git@sicet7.com>
2025-12-04 23:52:33 +0100

    WIP.

--- config/users/sicet7/imports/xterm.nix
+++ config/users/sicet7/imports/xterm.nix
@@ -7,19 +7,36 @@
   ];
 
   home-manager.users.sicet7 = {
+    programs.kitty = {
+      enable = true;
+      font = {
+        name = "SauceCodePro Nerd Font Mono";
+        size = 12;
+      };
+      settings = {
+        # Close window as soon as the shell/command exits
+        confirm_os_window_close = 0;
+
+        # Some nice defaults
+        shell_type = "login";
+        scrollback_lines = 10000;
+        window_padding_width = 6;
+      };
+      extraConfig = ''
+        background   #1e1e1e
+        foreground   #d4d4d4
+        selection_background #264f78
+        selection_foreground #ffffff
+
+        cursor              #aeafad
+        cursor_text_color   background
+      '';
+    };
 
     dconf.settings = {
       "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal" = {
         binding = "<Control><Alt>t";
-        command = ''
-          xterm \
-            -xrm "XTerm*faceName: SauceCodePro Nerd Font Mono" \
-            -xrm "XTerm*faceSize: 12" \
-            -xrm "XTerm*Background: Grey19" \
-            -xrm "XTerm*Foreground: white" \
-            -xrm "XTerm*VT100*selectToClipboard: true" \
-            -e tmux
-        '';
+        command = "kitty -e tmux";
         name = "Open Terminal";
       };
       "org/gnome/settings-daemon/plugins/media-keys" = {
