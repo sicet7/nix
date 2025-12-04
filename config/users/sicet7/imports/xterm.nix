3e9e583dd4a38f5a520d5675b27505d0fac4321d
Martin René Sørensen <git@sicet7.com>
2025-12-04 23:30:42 +0100

    WIP.

--- config/users/sicet7/imports/xterm.nix
+++ config/users/sicet7/imports/xterm.nix
@@ -6,14 +6,20 @@
     ./tmux.nix
   ];
 
-  services.xserver.desktopManager.xterm.enable = true;
-
   home-manager.users.sicet7 = {
 
     dconf.settings = {
       "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal" = {
         binding = "<Control><Alt>t";
-        command = "xterm -e tmux";
+        command = ''
+          xterm \
+            -xrm "XTerm*faceName: SauceCodePro Nerd Font Mono" \
+            -xrm "XTerm*faceSize: 12" \
+            -xrm "XTerm*Background: Grey19" \
+            -xrm "XTerm*Foreground: white" \
+            -xrm "XTerm*VT100*selectToClipboard: true" \
+            -e tmux
+        '';
         name = "Open Terminal";
       };
       "org/gnome/settings-daemon/plugins/media-keys" = {
@@ -23,12 +29,4 @@
       };
     };
   };
-
-  xresources.extraConfig = ''
-    XTerm*faceName: SauceCodePro Nerd Font Mono
-    XTerm*faceSize: 12
-    XTerm*Background: Grey19
-    XTerm*Foreground: white
-    XTerm*VT100*selectToClipboard: true
-  '';
 }
