e27d9f3399bedd4b42724e54f0038aeca4973c9b
Martin René Sørensen <git@sicet7.com>
2025-12-25 21:42:41 +0100

    WIP.

--- config/users/sicet7/imports/shells/node/20.nix
+++ config/users/sicet7/imports/shells/node/20.nix
@@ -1,3 +1,8 @@
+let
+  pkgs = import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/refs/heads/release-25.11.tar.gz";
+  }) {};
+in
 pkgs.buildEnv {
   name = "node20-env";
   paths = [
