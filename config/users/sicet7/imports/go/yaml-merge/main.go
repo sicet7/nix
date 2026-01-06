086040a5e8c02960aad4f70a9c476ff6987eaf5b
Martin René Sørensen <git@sicet7.com>
2026-01-06 20:00:52 +0100

    Updated yaml-merge.

--- config/users/sicet7/imports/go/yaml-merge/main.go
+++ config/users/sicet7/imports/go/yaml-merge/main.go
@@ -8,21 +8,29 @@ import (
 )
 
 func usage() {
-	fmt.Fprintln(os.Stderr, "Usage: yaml-merge <file1.yaml> <file2.yaml> ...")
+	fmt.Fprintln(os.Stderr, "Usage: yaml-merge [--append-slices] <file1.yaml> <file2.yaml> ...")
 	fmt.Fprintln(os.Stderr, "Deep-merges YAML maps left-to-right; later files override earlier ones.")
+	fmt.Fprintln(os.Stderr, "  --append-slices   If both values are YAML sequences, append instead of overriding.")
 	os.Exit(2)
 }
 
-func deepMerge(dst, src map[string]any) map[string]any {
+func deepMerge(dst, src map[string]any, appendSlices bool) map[string]any {
 	for k, sv := range src {
 		if dv, ok := dst[k]; ok {
 			dm, dok := dv.(map[string]any)
 			sm, sok := sv.(map[string]any)
 			if dok && sok {
-				dst[k] = deepMerge(dm, sm)
+				dst[k] = deepMerge(dm, sm, appendSlices)
 				continue
 			}
-			// everything else: override
+
+			ds, dok := dv.([]any)
+			ss, sok := sv.([]any)
+			if appendSlices && dok && sok {
+				dst[k] = append(ds, ss...)
+				continue
+			}
+
 			dst[k] = sv
 		} else {
 			dst[k] = sv
@@ -36,15 +44,28 @@ func main() {
 	if len(args) < 1 {
 		usage()
 	}
+
+	appendSlices := false
+	files := make([]string, 0, len(args))
+
 	for _, a := range args {
-		if a == "-h" || a == "--help" {
+		switch a {
+		case "-h", "--help":
 			usage()
+		case "--append-slices":
+			appendSlices = true
+		default:
+			files = append(files, a)
 		}
+	}
+
+	if len(files) == 0 {
+		usage()
 	}
 
 	merged := map[string]any{}
 
-	for i, file := range args {
+	for i, file := range files {
 		b, err := os.ReadFile(file)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "yaml-merge: unable to read file %d (%s): %v\n", i, file, err)
@@ -57,7 +78,7 @@ func main() {
 			os.Exit(1)
 		}
 
-		merged = deepMerge(merged, m)
+		merged = deepMerge(merged, m, appendSlices)
 	}
 
 	out, err := yaml.Marshal(merged)
