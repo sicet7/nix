e064653748be4b8e035aeba5f4ea23fdcd8925c0
Martin René Sørensen <git@sicet7.com>
2026-03-04 23:13:10 +0100

    Added proton lumo web app

--- config/general/proton.nix
+++ config/general/proton.nix
@@ -3,5 +3,6 @@
   imports = [
     ./proton/mail.nix
     ./proton/pass.nix
+    ./proton/lumo.nix
   ];
 }
---
+++ config/general/proton/lumo.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, ... }:
+let
+  browser = pkgs.ungoogled-chromium;
+  protonLumoSvg = builtins.path {
+    path = ./icons/lumo.svg;
+    name = "proton-lumo.svg";
+  };
+  protonLumoWrapper = pkgs.writeShellScriptBin "proton-lumo" ''
+    exec ${browser}/bin/${browser.meta.mainProgram} \
+      --ozone-platform=x11 \
+      --class=proton-lumo \
+      --profile-directory="proton-lumo-profile" \
+      --force-dark-mode \
+      --no-first-run \
+      --no-default-browser-check \
+      --app=https://lumo.proton.me/ "$@"
+  '';
+  protonLumoDesktop = pkgs.makeDesktopItem {
+      name = "proton-lumo";
+      exec = "${protonLumoWrapper}/bin/proton-lumo";
+      icon = "${protonLumoSvg}";
+      comment = "zero-access encrypted AI assistant by Proton";
+      desktopName = "Proton Lumo";
+      categories = [ "Network" "AI" ];
+      startupWMClass = "proton-lumo";
+      extraConfig = {
+        "X-AppId" = "proton-lumo";
+      };
+      type = "Application";
+    };
+in {
+  environment.systemPackages = [
+    browser
+    protonLumoWrapper
+    protonLumoDesktop
+  ];
+}
